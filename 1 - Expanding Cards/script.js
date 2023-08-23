77d35c26d4192ab667d07872cd30dfd49b558c6b
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-08-23 12:01:41 +0300

    Final commit

--- 1 - Expanding Cards/script.js
+++ 1 - Expanding Cards/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const panels = document.querySelectorAll(".panel");
+const removeActiveClasses = function () {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+panels.forEach((panel) => {
+  panel.addEventListener("click", function () {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+// for (let i = 0; i < 5; i++) {
+//   panel[i].addEventListener("click", function () {
+//     panel[i].classList.toggle("active");
+//   });
+// }
